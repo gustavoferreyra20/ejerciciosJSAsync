e67688dd657549516e81de04e776ca2c9f083b8a
gustavoferreyra20 <ferreyragustavo847@gmail.com>
2020-09-30 22:39:43 -0300

    Ejercicios completos

--- ejercicio_5/app.js
+++ ejercicio_5/app.js
@@ -3,9 +3,26 @@ const obtenerPersonaFake = require("./library");
 
 // Codigo funcion callback
 function funcionCallback(personaExtraida){
-    console.log(personaExtraida.results);
+    console.log(datosPedidos(personaExtraida));
 }
 
+function datosPedidos(personaRecibida){
+    var resultadosPersona = personaRecibida.results[0];
+    var persona ={
+        nombre: resultadosPersona.name.title + "." + resultadosPersona.name.first + " " + resultadosPersona.name.last,
+        genero: resultadosPersona.gender,
+        pais: resultadosPersona.location.country,
+        estado: resultadosPersona.location.state,
+        ciudad: resultadosPersona.location.city,
+        calle: resultadosPersona.location.street.name,
+        numero: resultadosPersona.location.street.number,
+        postcode: resultadosPersona.location.postcode,
+        email: resultadosPersona.email,
+        username: resultadosPersona.login.username,
+        password: resultadosPersona.login.password,
+    };
+    return persona;
+}
 
 // Fin codigo
 
--- ejercicio_6/app.js
+++ ejercicio_6/app.js
@@ -2,4 +2,26 @@
 const obtenerPersonaFake = require("./library");
 
 // Usar la funcion obtenerPersonaFake() la cual devuelve la promesa de traer el objeto persona extraido
-obtenerPersonaFake().then(x => console.log(x));
+obtenerPersonaFake().then(function(personaExtraida) {
+    return datosPedidos(personaExtraida);
+}).then(x => console.log(x)).catch(function(err) {
+    console.log("Fetch error: "+ err.message);
+})
+
+function datosPedidos(personaRecibida){
+    var resultadosPersona = personaRecibida.results[0];
+    var persona ={
+        nombre: resultadosPersona.name.title + "." + resultadosPersona.name.first + " " + resultadosPersona.name.last,
+        genero: resultadosPersona.gender,
+        pais: resultadosPersona.location.country,
+        estado: resultadosPersona.location.state,
+        ciudad: resultadosPersona.location.city,
+        calle: resultadosPersona.location.street.name,
+        numero: resultadosPersona.location.street.number,
+        postcode: resultadosPersona.location.postcode,
+        email: resultadosPersona.email,
+        username: resultadosPersona.login.username,
+        password: resultadosPersona.login.password,
+    };
+    return persona;
+}
